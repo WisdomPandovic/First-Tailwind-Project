810ff163775510d5f17f82cab5b7aed3673302ac
WisdomPandovic <wisdomokezieonwuchekwa@gmail.com>
2024-01-11 09:52:45 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('toggleBtn').addEventListener('click', function () {
+  var dropdownMenu = document.getElementById('dropdownMenu');
+  dropdownMenu.classList.toggle('hidden');
+});
